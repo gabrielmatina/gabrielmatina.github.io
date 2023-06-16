9be8ede73176382085650fe32b1ab1d82d8d1353
gabrielmatina <gabrielmatina@hotmail.com>
2023-06-16 08:29:35 -0300

    :sparkles: feat: atualiza responsividade

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var headers = document.querySelectorAll('.accordion-header');
+
+    headers.forEach(function(header) {
+        header.addEventListener('click', function() {
+            var content = this.nextElementSibling;
+
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
+        });
+    });
+});
